6aa456ad2426b04f885c451a5e7775476bc76229
Agkdesign <anthonykamil@hotmail.com>
2016-12-07 22:18:06 +0000

    Add files via upload

--- www/index.js
+++ www/index.js
@@ -15,7 +15,7 @@
 // DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
 // UNINTERRUPTED OR ERROR FREE.
 /////////////////////////////////////////////////////////////////////////////////
-var defaultUrn = '<replace with your encoded urn>';
+var defaultUrn = '<dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE2LTEyLTA1LTE0LTMwLTI0LWx3ZjRyZmdzZWN4cno5OG1uc2h4OHZ6b2g5ZG8vUlNRLUI0MjEwLU1ETC1SRVMtTDA4LU0zLVgtMDAwMy1XSVBfaXNzMV9yZXYubndk>';
 
 $(document).ready(function () {
     var tokenurl = 'http://' + window.location.host + '/api/token';
